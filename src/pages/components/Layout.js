76df4f529d2c05c93dae438c725eff7e2c4034ec
SourceH <zizark4040@gmail.com>
2023-07-11 20:05:41 +0900

    Update

---
+++ src/pages/components/Layout.js
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+
+export default function Layout() {
+  return (
+    <>
+      <header className="py-4">
+        <div className="container mx-auto flex justify-between items-center px-4">
+
+          {/* 로고 */}
+          <div className="flex items-center">
+            <Image src="/logo_white.png" alt="Logo" width={80} height={30} />
+          </div>
+
+          {/* 검색창 */}
+          <form className="flex items-center justify-center flex-grow">
+            <input
+              className="border border-gray-800 rounded-full py-2 px-4 w-1/2 bg-[#202026]"
+              type="text"
+              placeholder="검색어를 입력하세요!"
+            />
+          </form>
+
+          {/* 로그인 */}
+          <div>
+            <button className="text-lg font-bold">로그인</button>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}

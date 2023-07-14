f2c405428c9cc616af7ece174c514b4d8b6bc43c
SourceH <zizark4040@gmail.com>
2023-07-14 21:47:19 +0900

    update

--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -9,17 +9,13 @@ export default function Layout() {
             <Image src="/logo_white.png" alt="Logo" width={80} height={30} />
           </div>
 
-          <form className="flex items-center justify-center flex-grow">
+          <form className="flex items-center justify-end flex-grow">
             <input
               className="border border-gray-800 rounded-full py-2 px-4 w-1/2 bg-[#202026]"
               type="text"
               placeholder="검색어를 입력하세요!"
             />
           </form>
-
-          <div>
-            <button className="text-lg font-bold">로그인</button>
-          </div>
         </div>
       </header>
     </>
